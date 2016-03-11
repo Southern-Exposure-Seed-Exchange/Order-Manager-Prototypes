7d5d6e2770581fd5e4feb837b9b9d3b43ba95602
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-03-10 21:21:44 -0500

    ember: Cleanup Adapter

--- om-ember/app/application/adapter.js
+++ om-ember/app/application/adapter.js
@@ -2,11 +2,11 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.RESTAdapter.extend({
-  // Spock
-  //host: 'http://localhost:8081',
-  headers: {
-    "Content-Type": 'application/json',
-    "Accept": "application/json",
-    "X-XSRF-TOKEN": Ember.get(document.cookie.match(/XSRF\-TOKEN\=([^;]*)/), "1"),
-  }
+  headers: Ember.computed(() => {
+    return {
+      "Content-Type": 'application/json',
+      "Accept": "application/json",
+      "X-XSRF-TOKEN": Ember.get(document.cookie.match(/XSRF\-TOKEN\=([^;]*)/), "1"),
+    };
+  }).volatile(),
 });
