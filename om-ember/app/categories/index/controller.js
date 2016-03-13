fe20ab076910776c80999e01253f7f9a40d08d1f
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-03-12 21:36:51 -0500

    ember: Filter Category Table using Property
    Modify the Category index page to pull all categories and show only a
    subset, instead of requesting only a subset of categories.

---
+++ om-ember/app/categories/index/controller.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  roots: Ember.computed.filter('model.@each.parent', function (category) {
+    return category.get('parent.id') == null;
+  }),
+});
--- om-ember/app/categories/route.js
+++ om-ember/app/categories/route.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    this.store.findAll('category');
-    return this.store.query('category', {filter: {parent: null}});
-  },
 });
