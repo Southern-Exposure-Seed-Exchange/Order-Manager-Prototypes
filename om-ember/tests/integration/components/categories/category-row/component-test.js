04e6177799b26cc787419439eff1c12f8238ff79
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-03-12 19:51:28 -0500

    ember: Put category-table Component Into Pod

--- om-ember/tests/integration/components/categories/category-row/component-test.js
+++ om-ember/tests/integration/components/categories/category-row/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('category-table', 'Integration | Component | category table', {
+moduleForComponent('categories/category-row', 'Integration | Component | categories/category row', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{category-table}}`);
+  this.render(hbs`{{categories/category-row}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#category-table}}
+    {{#categories/category-row}}
       template block text
-    {{/category-table}}
+    {{/categories/category-row}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
