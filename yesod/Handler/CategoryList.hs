eaf4a261e8161c6bbfcf8e83d9cea1e27a3623d3
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-03-12 19:35:19 -0500

    yesod: Add Filter & Relations to CategoryList
    * Modify the getCategoryListR handler to allow filtering by a parent id.
    * Modify the getCategoryListR handler to return a list of sideloaded
      Products.

--- yesod/Handler/CategoryList.hs
+++ yesod/Handler/CategoryList.hs
@@ -1,11 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Handler.CategoryList where
 
 import Import
+import Database.Persist.Sql (toSqlKey)
+import Prelude (read)
 
 getCategoryListR :: Handler Value
 getCategoryListR = do
-        cats <- runDB (selectList [] [Desc CategoryName])
-        returnJson $ object ["category" .= cats]
+        filters <- createFilterList $ zip filterParams filterHandlers
+        cats <- runDB $ selectList filters [Asc CategoryName]
+        (prods :: [Entity Product]) <- runDB $ selectList [] []
+        returnJson $ object ["category" .= cats, "products" .= prods]
+        where filterParams = ["parent"]
+              filterHandlers = [parentFilter]
+              parentFilter "" = [CategoryParent ==. Nothing]
+              parentFilter s  =
+                case read (unpack s) of
+                    Nothing -> []
+                    Just i  -> [CategoryParent ==. Just (toSqlKey i)]
+
+createFilterList :: [(Text, Text -> [Filter val])] -> Handler [Filter val]
+createFilterList = foldM applyFilter []
+        where filterParam param = concat ["filter[", param, "]"]
+              applyFilter acc (name, handler) = do
+                    value <- lookupGetParam $ filterParam name
+                    case value of
+                        Nothing -> return acc
+                        Just x  -> return (handler x <> acc)
+
 
 postCategoryListR :: Handler Value
 postCategoryListR = do
