e277e0efbf88b75dc394719490cda51d3919ce42
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-03-15 21:32:01 -0400

    ember: Fix Select
    Move the Categories API request to the root categories route instead of
    the index route so all the Categories are available to all sub-routes.
    
    This fixes the empty "Parent" select box when directly visiting a
    Categories edit page.

--- om-ember/app/categories/base/controller.js
+++ om-ember/app/categories/base/controller.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  categories: Ember.computed({get() { return this.store.findAll('category'); }}),
+  categories: Ember.computed({get() { return this.store.peekAll('category'); }}),
   validParents: Ember.computed.filter('categories',
     function(category) {
       return category.get('id') !== this.get('model.id');
