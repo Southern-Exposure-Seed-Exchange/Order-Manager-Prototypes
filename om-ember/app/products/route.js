64ca248ff25ea250fafbb63975b17d845002a0d6
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-03-16 23:48:27 -0400

    ember: Only Show Saved Instances in Products Table
    * Move products API request to the base Products route.
    * Add a 'createdProducts' property to the base Products route, to filter
      out new products awaiting their initial save.

--- om-ember/app/products/route.js
+++ om-ember/app/products/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('product');
+  },
+  setupController(model, controller) {
+    this._super(model, controller);
+    controller.set(
+      'createdProducts', Ember.computed.filterBy('model', 'isNew', false));
+  },
 });
