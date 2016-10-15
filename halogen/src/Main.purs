90ee4bedec973785264755a855cc222bfe9c2cbc
Pavan Rikhi <pavan.rikhi@gmail.com>
2016-10-15 08:36:43 -0400

    halogen: Add Basic Category List

--- halogen/src/Main.purs
+++ halogen/src/Main.purs
@@ -2,7 +2,17 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Decode (class DecodeJson, (.?), decodeJson)
+import Data.Array (fromFoldable)
+import Data.Either (Either(..))
+import Data.List (List(Nil))
+import Data.Maybe (Maybe)
+import Data.MediaType.Common (applicationJSON)
+import Network.HTTP.Affjax (AffjaxResponse, AJAX, affjax, defaultRequest)
+import Network.HTTP.RequestHeader (RequestHeader(..))
 
 import Halogen as H
 import Halogen.HTML.Events.Indexed as HE
@@ -11,41 +21,103 @@ import Halogen.Util (awaitBody, runHalogenAff)
 
 
 data Query a
-    = ToggleState a
+    = FetchCategories a
+
+
+data Category = Category
+    { id :: Int
+    , name :: String
+    , description :: String
+    , parent :: Maybe Int
+    }
+
+
+instance decodeJsonCategory :: DecodeJson Category where
+    decodeJson json = do
+        obj <- decodeJson json
+        id <- obj .? "id"
+        name <- obj .? "name"
+        description <- obj .? "description"
+        parent <- obj .? "parent"
+        pure $ Category { id, name, description, parent }
+
+
+data CategoryData = CategoryData
+    { categories :: List Category
+    }
+
+
+decodeCategoryData :: AffjaxResponse Json -> Either String CategoryData
+decodeCategoryData res = do
+        obj <- decodeJson res.response
+        categories <- obj .? "category" >>= decodeJson
+        pure $ CategoryData { categories }
 
 
 type State =
-    { on :: Boolean
-    }
+    CategoryData
 
 
 initialState :: State
-initialState =
-    { on: false
+initialState = CategoryData
+    { categories: Nil
     }
 
 
-ui :: forall g. H.Component State Query g
+type AppEffects e = H.HalogenEffects ( ajax :: AJAX | e )
+
+
+ui :: forall e. H.Component State Query (Aff (AppEffects e))
 ui = H.component { render, eval }
     where render :: State -> H.ComponentHTML Query
-          render state =
+          render (CategoryData state) =
               HH.div_
-                [ HH.h1_ [ HH.text "Hello World!" ]
-                , HH.p_ [ HH.text "Why not toggle this button:" ]
+                [ HH.h1_ [ HH.text "Categories" ]
                 , HH.button
-                    [ HE.onClick (HE.input_ ToggleState) ]
-                    [ HH.text
-                        if not state.on
-                        then "Don't push me"
-                        else "I said don't push me!"
-                    ]
+                    [ HE.onClick (HE.input_ FetchCategories) ]
+                    [ HH.text "Fetch" ]
+                , categoryTable state.categories
                 ]
-          eval :: Query ~> H.ComponentDSL State Query g
-          eval (ToggleState next) = do
-              H.modify (\state -> { on: not state.on })
+          categoryTable :: List Category -> H.ComponentHTML Query
+          categoryTable categories =
+              HH.table_
+                [ HH.thead_
+                    [ HH.tr_
+                        [ HH.th_ [ HH.text "Name" ]
+                        , HH.th_ [ HH.text "Categories" ]
+                        , HH.th_ [ HH.text "Products" ]
+                        ]
+                    ]
+                , HH.tbody_ $ fromFoldable $ map categoryRow categories
+                ]
+          categoryRow :: Category -> H.ComponentHTML Query
+          categoryRow (Category category) =
+              HH.tr_
+                [ HH.td_ [ HH.text category.name ]
+                , HH.td_ [ HH.text "0" ]
+                , HH.td_ [ HH.text "0" ]
+                ]
+          eval :: Query ~> H.ComponentDSL State Query (Aff (AppEffects e))
+          eval (FetchCategories next) = do
+              result <- H.fromAff fetchCategories
+              H.modify (\state -> result)
               pure next
 
-main :: Eff (H.HalogenEffects ()) Unit
+
+fetchCategories :: forall e. Aff (ajax :: AJAX | e) CategoryData
+fetchCategories = do
+    result <- affjax $ defaultRequest
+        { url = "http://localhost:3000/categories"
+        , headers = [ ContentType applicationJSON, Accept applicationJSON ]
+        }
+    cats <- pure $ do
+        decodeCategoryData result
+    pure $ case cats of
+        Right cs -> cs
+        Left err -> CategoryData { categories: Nil }
+
+
+main :: Eff (H.HalogenEffects (ajax :: AJAX)) Unit
 main = runHalogenAff do
   body <- awaitBody
   H.runUI ui initialState body
